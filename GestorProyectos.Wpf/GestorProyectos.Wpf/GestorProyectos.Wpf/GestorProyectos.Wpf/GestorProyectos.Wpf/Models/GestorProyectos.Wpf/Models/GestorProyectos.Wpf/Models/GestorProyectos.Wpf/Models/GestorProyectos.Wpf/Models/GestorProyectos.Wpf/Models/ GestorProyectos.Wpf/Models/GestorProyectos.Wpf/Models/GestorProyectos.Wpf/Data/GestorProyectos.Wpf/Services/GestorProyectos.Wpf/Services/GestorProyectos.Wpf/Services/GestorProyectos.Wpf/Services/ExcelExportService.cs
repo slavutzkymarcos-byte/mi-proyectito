63bc7dc79c117ef57af8c6dec66dc87d3cd4072c
slavutzkymarcos-byte <slavutzkymarcos@gmail.com>
2025-12-11 16:55:04 +0200

    Create ExcelExportService.cs

---
+++ GestorProyectos.Wpf/GestorProyectos.Wpf/GestorProyectos.Wpf/GestorProyectos.Wpf/GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Models/ GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Models/GestorProyectos.Wpf/Data/GestorProyectos.Wpf/Services/GestorProyectos.Wpf/Services/GestorProyectos.Wpf/Services/GestorProyectos.Wpf/Services/ExcelExportService.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using OfficeOpenXml;
+
+namespace GestorProyectos.Wpf.Services
+{
+    public class ExcelExportService : IExcelExportService
+    {
+        public Excel
